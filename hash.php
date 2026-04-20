293a492a3cd700052f2d9fd16db03604493eba82
Myat ThiHa Tun <thihagongar7@example.com>
2026-04-20 19:50:26 +0630

    final

--- hash.php
+++ hash.php
@@ -1,4 +1,6 @@
 <?php
 echo password_hash("710200478", PASSWORD_DEFAULT);
-
+echo password_hash("200478", PASSWORD_DEFAULT);
+echo password_hash("superuser", PASSWORD_DEFAULT);
 ?>
+<!-- what ever this is all my pj password so if you want to try use it -->
